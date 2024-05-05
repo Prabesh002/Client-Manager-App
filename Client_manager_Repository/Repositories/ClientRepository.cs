85cd495dc3e1b1e7d84761707a38e803f1407399
Prabesh002 <149858321+Prabesh002@users.noreply.github.com>
2024-05-05 12:42:08 +0545

    up

--- Client_manager_Repository/Repositories/ClientRepository.cs
+++ Client_manager_Repository/Repositories/ClientRepository.cs
@@ -8,62 +8,63 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Client_Manager_Repository.Repositories
 {
-    public class ClientRepository : IClientRepository
-    {
-        private readonly AppDatabase _context;
+	public class ClientRepository : IClientRepository
+	{
+		private readonly AppDatabase _context;
 
-        public ClientRepository(AppDatabase context)
-        {
-            _context = context;
-        }
+		public ClientRepository(AppDatabase context)
+		{
+			_context = context;
+		}
 
-        public async Task<IQueryable<ClientModel>> GetAllClientsQueryableAsync()
-        {
-            return await Task.FromResult(_context.Clients.AsQueryable());
-        }
+		public async Task<IQueryable<ClientModel>> GetAllClientsQueryableAsync()
+		{
+			return await Task.FromResult(_context.Clients.AsQueryable());
+		}
 
-        public async Task<ClientModel> GetClientByIdAsync(int id)
-        {
-            return await _context.Clients.FindAsync(id);
-        }
+		public async Task<ClientModel> GetClientByIdAsync(int id)
+		{
+			return await _context.Clients.FindAsync(id);
+		}
 
-        public async Task<List<ClientModel>> SearchClientsAsync(string searchTerm)
-        {
-            return await _context.Clients
-                .Where(client => client.Name.Contains(searchTerm))
-                .ToListAsync();
-        }
+		public async Task<List<ClientModel>> SearchClientsAsync(string searchTerm)
+		{
+			return await _context.Clients
+				.Where(client => client.Name.Contains(searchTerm))
+				.ToListAsync();
+		}
 
-        public async Task<List<ClientModel>> GetClientsByTypeAsync(ClientType type)
-        {
-            return await _context.Clients.Where(c => c.ClientType == type).ToListAsync();
-        }
+		public async Task<List<ClientModel>> GetClientsByTypeAsync(ClientType type)
+		{
+			return await _context.Clients.Where(c => c.ClientType == type).ToListAsync();
+		}
 
-        public async Task<List<ClientModel>> GetFilteredClientsAsync(string searchTerm, string filterBy)
-        {
-            IQueryable<ClientModel> query = _context.Clients;
+		public async Task<List<ClientModel>> GetFilteredClientsAsync(string searchTerm, string filterBy)
+		{
+			IQueryable<ClientModel> query = _context.Clients;
 
-            if (!string.IsNullOrEmpty(searchTerm) && !string.IsNullOrEmpty(filterBy))
-            {
-                switch (filterBy)
-                {
-                    case "Name":
-                        query = query.Where(c => c.Name.Contains(searchTerm));
-                        break;
-                    case "Email":
-                        query = query.Where(c => c.Email.Contains(searchTerm));
-                        break;
-                    case "MaxOffer":
-                        query = query.Where(c => c.MaxOffer.Contains(searchTerm));
-                        break;
-                    default:
-                        break;
-                }
-            }
+			if (!string.IsNullOrEmpty(searchTerm) && !string.IsNullOrEmpty(filterBy))
+			{
+				switch (filterBy)
+				{
+					case "Name":
+						query = query.Where(c => c.Name.Contains(searchTerm));
+						break;
+					case "Email":
+						query = query.Where(c => c.Email.Contains(searchTerm));
+						break;
+					case "MaxOffer":
+						query = query.Where(c => c.MaxOffer.Contains(searchTerm));
+						break;
+					default:
+					   query = query.Where(c => c.Name.Contains(searchTerm));
+						break;
+				}
+			}
 
-            return await query.ToListAsync();
-        }
+			return await query.ToListAsync();
+		}
 
-       
-    }
+	   
+	}
 }
